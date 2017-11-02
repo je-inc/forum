979dfee3418f3fd48c8dc3ea15b21cd2b11cbe56
Reznikov Evgeniy <je_inc@ukr.net>
2017-11-02 21:56:50 +0200

    Add navbar

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+# def board_topics(request, pk):
+#     board = Board.objects.get(pk=pk)
+#     return render(request, 'topics.html', {'board': board})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
